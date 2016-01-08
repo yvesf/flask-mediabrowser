807a161a937459c7d8b2d479e12f42ff3da8b96c
Yves Fischer <yvesf-git@xapek.org>
2016-01-08 20:30:36 +0100

    fix packaging

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(name='flask-mediabrowser',
       author_email='yvesf+mediabrowser@xapek.org',
       url='https://www.xapek.org/git/yvesf/flask-mediabrowser',
       packages=['mediabrowser'],
+      include_package_data=True,  # use MANIFEST.in during install
       scripts=['flask-mediabrowser'],
       install_requires=['Flask==0.10.1']
       )

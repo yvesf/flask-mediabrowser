500cd0cb43dce858a7ed22ea33d9557068cbfae1
Yves Fischer <yvesf-git@xapek.org>
2016-05-26 20:25:47 +0200

    add a test

--- test/ffmpeg.py
+++ test/ffmpeg.py
@@ -1,6 +1,7 @@
 import mediabrowser.ffmpeg as ffmpeg
 
 import os
+import tempfile
 from pprint import pprint
 from unittest import TestCase
 
@@ -24,3 +25,20 @@ class TestFfmpeg(TestCase):
     def test_calculate_splittimes(self):
         for time in ffmpeg.calculate_splittimes(self.test_mkv, 120):
             print(time)
+
+    def test_split(self):
+        _, pos1 = ffmpeg.find_next_keyframe(self.test_mkv, 2, 5)
+        frame_dur1, pos2 = ffmpeg.find_next_keyframe(self.test_mkv, pos1+2, 5)
+        frame_dur2, pos3 = ffmpeg.find_next_keyframe(self.test_mkv, pos2+2, 5)
+        out1 = open(tempfile.mktemp(), "wb")
+        out2 = open(tempfile.mktemp(), "wb")
+        pprint(locals())
+
+        out1.write(ffmpeg.stream(self.test_mkv, pos1, pos2-pos1-frame_dur1).stdout.read())
+        out2.write(ffmpeg.stream(self.test_mkv, pos2, pos3-pos2-frame_dur2).stdout.read())
+
+        
+
+        os.remove(out1.name)
+        os.remove(out2.name)
+
